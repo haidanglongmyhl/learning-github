dc8510ffe247e0f1a0d06d7f05f92c72360eadea
Your Name <you@example.com>
2025-02-27 16:02:51 +0700

    delete order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { OrdersService } from './providers/orders.service';
 import { CreateOrderDto } from './dtos/create-order.dto';
 
@@ -9,5 +9,15 @@ export class OrdersController {
   @Post('Create')
   public async createOrder(@Body() createOrderDto: CreateOrderDto) {
     return await this.orderService.createOrder(createOrderDto);
+  }
+
+  @Get('list')
+  public async getProducts() {
+    return await this.orderService.listOrder();
+  }
+
+  @Delete('delete')
+  public deletePost(@Query('id', ParseIntPipe) id: number) {
+    return this.orderService.delete(id);
   }
 }
